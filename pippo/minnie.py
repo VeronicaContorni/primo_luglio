deffc4a153d76da786253e8b58744ac2a06671cf
VeronicaContorni <veronica.contorni@student.unisi.it>
2020-07-01 15:23:23 +0000

    Modifica a minnie

--- pippo/minnie.py
+++ pippo/minnie.py
@@ -4,4 +4,13 @@ import time
 ACCELEROMETER_FILE = '/sensors/accelerometer/data'
 
 
-f=open ()
+f=open (ACCELEROMETER_FILE)
+a=f.readline()
+lista=a.split(",")
+
+z = int(lista[2])
+
+if z > 0: 
+    print('orizzontale')
+else: 
+    print ('verticale')
